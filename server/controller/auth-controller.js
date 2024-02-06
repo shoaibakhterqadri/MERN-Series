c41ac41ca7a0ec016f851390dfdd4d41f9f1d0cb
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-02-06 15:54:47 +0500

    post data in database by the help of express js

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,4 +1,4 @@
-const express=require('express');
+const User=require('../models/user-model')
 
 
 const login= async (req,res)=>{
@@ -12,9 +12,18 @@ const login= async (req,res)=>{
 const register = async (req,res)=>{
 try {
     console.log(req.body);
-    res.status(200).send({message:req.body})
+    const {username,email,phone,password}=req.body;
+
+    const userExist=await User.findOne({email});
+    if(userExist){
+        return res.status(400).json({msg:"User is already exist"});
+    }
+
+    const user=await User.create({username,email,phone,password})
+
+    return res.status(200).json({msg:user})
 } catch (error) {
-    
+    console.log("Register Error "+error);
 }
 }
 
