af3b9504cb7907b78128430522792dd123d23336
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-02-07 10:51:59 +0500

    password is bcrypted

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,28 +1,45 @@
-const mongoose=require('mongoose');
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    phone:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    isAdmin:{
-        type:Boolean,
-        required:false
-    }
-})
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: false,
+  },
+});
 
-const user=mongoose.model("User",userSchema);
+userSchema.pre("save", async function (next) {
+  const user = this;
 
-module.exports=user
+  if (!user.isModified("password")) {
+    next();
+  }
+
+  try {
+    const saltRound = await bcrypt.genSalt(10);
+    const hash_password = await bcrypt.hash(user.password,saltRound);
+   user.password = hash_password
+  } catch (error) {
+    console.log("Salt failed: " + error.message);
+  }
+});
+
+const user = mongoose.model("User", userSchema);
+
+module.exports = user;
