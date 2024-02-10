bc819270bf440b0b2818dc965d2d676dc03af654
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-02-10 14:54:36 +0500

    Register page is completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+     <h1>Register</h1>
+    <form onSubmit={handleForm}>
+        <label htmlFor="username">Username: </label>
+        <input type='text' name='username' id='username' value={user.username} onChange={handleInput} /><br/>
+
+        <label htmlFor="email">Email: </label>
+        <input type='email' name='email' id='email' value={user.email} onChange={handleInput} /><br/>
+
+        <label htmlFor="phone">Phone: </label>
+        <input type='text' name='phone' id='phone' value={user.phone} onChange={handleInput} /><br/>
+
+        <label htmlFor="password">Password: </label>
+        <input type='password' name='password' id='password' value={user.password} onChange={handleInput} /><br/>
+        <button type='submit'>Submit</button>
+    </form>
+    </>
   )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Register() {
+    const [user,setUser]=useState({
+        username:"",
+        email:"",
+        phone:"",
+        password:""
+    })
+
+    const handleInput=(e)=>{
+        let name=e.target.name;
+        let value=e.target.value;
+        setUser({
+            ...user,
+            [name]:value,
+        })
+    }
+    
+    const handleForm=(e)=>{
+        e.preventDefault();
+    }
   return (
-    <div>Register</div>
+    <>
+    <h1>Register</h1>
+    <form onSubmit={handleForm}>
+        <label htmlFor="username">Username: </label>
+        <input type='text' name='username' id='username' value={user.username} onChange={handleInput} /><br/>
+
+        <label htmlFor="email">Email: </label>
+        <input type='email' name='email' id='email' value={user.email} onChange={handleInput} /><br/>
+
+        <label htmlFor="phone">Phone: </label>
+        <input type='text' name='phone' id='phone' value={user.phone} onChange={handleInput} /><br/>
+
+        <label htmlFor="password">Password: </label>
+        <input type='password' name='password' id='password' value={user.password} onChange={handleInput} /><br/>
+        <button type='submit'>Submit</button>
+    </form>
+    </>
   )
 }
 
